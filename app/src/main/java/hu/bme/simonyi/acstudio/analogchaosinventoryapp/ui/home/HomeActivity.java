48c5e72814ee08239d84c8a821f0ca8422bf16d0
Lajos Nyeki <nyekilajos@gmail.com>
2015-05-19 23:58:09 +0200

    Actionbar behaviour for navigation drawer fixed. Sleep removed which was added for debugging purposes.

--- app/src/main/java/hu/bme/simonyi/acstudio/analogchaosinventoryapp/ui/home/HomeActivity.java
+++ app/src/main/java/hu/bme/simonyi/acstudio/analogchaosinventoryapp/ui/home/HomeActivity.java
@@ -2,7 +2,9 @@ package hu.bme.simonyi.acstudio.analogchaosinventoryapp.ui.home;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 
 import hu.bme.simonyi.acstudio.analogchaosinventoryapp.R;
@@ -19,6 +21,7 @@ import roboguice.inject.InjectView;
 public class HomeActivity extends RoboActionBarActivity {
 
     private NavigationDrawerFragment drawerFragment;
+    private ActionBarDrawerToggle drawerToggle;
 
     @InjectView(R.id.drawer_layout)
     private DrawerLayout drawerLayout;
@@ -26,26 +29,40 @@ public class HomeActivity extends RoboActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try {
-            Thread.sleep(3000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         setContentView(R.layout.activity_home);
+        initDrawer();
+        swapFragment(ScanFragment.TAG, ScanFragment.TITLE_ID);
+    }
+
+    private void initDrawer() {
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer_content_desc, R.string.close_drawer_content_desc);
+        drawerLayout.setDrawerListener(drawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setDrawerItemSelectedListener(new NavigationDrawerFragment.DrawerItemSelectedListener() {
             @Override
             public void onDrawerItemSelected(View view, int position) {
-                swapFragment(NavigationDrawerDataProvider.getDrawerItemForPosition(position).getOpeningFragmentTag());
+                DrawerItem drawerItem = NavigationDrawerDataProvider.getDrawerItemForPosition(position);
+                swapFragment(drawerItem.getOpeningFragmentTag(), drawerItem.getTitleId());
             }
         });
-        swapFragment(ScanFragment.TAG);
     }
 
-    private void swapFragment(String openingFragmentTag) {
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return drawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    private void swapFragment(String openingFragmentTag, int titleId) {
         RoboFragment fragment = HomeScreenFragmentFactory.getFragmentByTag(openingFragmentTag);
         getSupportFragmentManager().beginTransaction().replace(R.id.container_body, fragment).commit();
-        getSupportActionBar().setTitle(openingFragmentTag);
+        getSupportActionBar().setTitle(getString(titleId));
         drawerLayout.closeDrawer(Gravity.LEFT);
     }
 
