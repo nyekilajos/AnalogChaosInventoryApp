9d7b2e1be0a0d3e3b899138635d6ad741e9d04f0
Lajos Nyeki <nyekilajos@gmail.com>
2015-06-06 14:35:06 +0200

    Some little UI based improvements.

--- app/src/main/java/hu/bme/simonyi/acstudio/analogchaosinventoryapp/ui/dialog/DialogFactory.java
+++ app/src/main/java/hu/bme/simonyi/acstudio/analogchaosinventoryapp/ui/dialog/DialogFactory.java
@@ -3,6 +3,10 @@ package hu.bme.simonyi.acstudio.analogchaosinventoryapp.ui.dialog;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.inject.Inject;
 
@@ -22,10 +26,21 @@ public class DialogFactory {
         this.context = context;
     }
 
+    /**
+     * Creates a custom alert dialog with a neutral OK button, circular alert icon and the given text.
+     *
+     * @param errorText The text to be shown on the alert dialog.
+     * @return The created AlertDialog object.
+     */
     public AlertDialog createAlertDialog(String errorText) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setIcon(R.drawable.alert_circle);
-        builder.setTitle(errorText);
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View dialogView = layoutInflater.inflate(R.layout.dialog_alert, null);
+        ImageView icon = (ImageView) dialogView.findViewById(R.id.icon_dialog);
+        icon.setImageResource(R.drawable.ic_alert_circle_grey600_48dp);
+        TextView tv = (TextView) dialogView.findViewById(R.id.tv_dialog_alert);
+        tv.setText(errorText);
+        builder.setView(dialogView);
         builder.setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
